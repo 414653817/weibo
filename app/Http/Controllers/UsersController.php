de9019e1c73e81b08581b660ab6642ae2d555b91
mahao <414653817@qq.com>
2019-03-08 04:11:23 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -25,6 +26,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        //自动登录
+        Auth::login($user);
         //当我们想存入一条缓存的数据，让它只在下一次的请求内有效时，则可以使用 flash 方法
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user->id]);
